d7232a709a815d12305fc0f200888ebc0e328cbb
Ishaan Gulrajani <igul222@gmail.com>
2012-11-10 10:47:32 -0500

    Client: add AFNetworking and empty DataProvider class

---
+++ client/Bestaurant/DataProvider.h
@@ -0,0 +1,13 @@
+//
+//  DataProvider.h
+//  Bestaurant
+//
+//  Created by Ishaan Gulrajani on 11/10/12.
+//  Copyright (c) 2012 HackPrinceton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataProvider : NSObject
+
+@end
---
+++ client/Bestaurant/DataProvider.m
@@ -0,0 +1,13 @@
+//
+//  DataProvider.m
+//  Bestaurant
+//
+//  Created by Ishaan Gulrajani on 11/10/12.
+//  Copyright (c) 2012 HackPrinceton. All rights reserved.
+//
+
+#import "DataProvider.h"
+
+@implementation DataProvider
+
+@end
